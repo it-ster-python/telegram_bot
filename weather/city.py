8115a47f4ac2f1d65dccf2cc750a8557682ff91e
Ваше Имя <hacker@google.com>
2019-08-13 19:52:06 +0300

    Update script

--- weather/city.py
+++ weather/city.py
@@ -17,13 +17,13 @@ def get_connect(path):
 
 def create_table(connect):
     sql = """CREATE TABLE IF NOT EXISTS "location" (
-    	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    	"country_name"	TEXT UNIQUE,
-    	"city_name"	TEXT,
-    	"country_code"	TEXT NOT NULL UNIQUE,
-    	"city"	TEXT NOT NULL,
-    	"lat"	REAL NOT NULL,
-    	"lon"	REAL NOT NULL
+        "id"    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        "country_name"    TEXT UNIQUE,
+        "city_name"    TEXT,
+        "country_code"    TEXT NOT NULL UNIQUE,
+        "city"    TEXT NOT NULL,
+        "lat"    REAL NOT NULL,
+        "lon"    REAL NOT NULL
     );"""
     cursor = connect.cursor()
     cursor.execute(sql)
@@ -35,7 +35,7 @@ def get_data(path):
     json_file.close()
     return data
 
-def send_data(element):
+def send_data(element, connect):
     # {'id': 707860, 'name': 'Hurzuf', 'country': 'UA', 'coord': {'lon': 34.283333, 'lat': 44.549999}}
     sql = f"""INSERT INTO "location" (
         "country_code",
@@ -43,14 +43,16 @@ def send_data(element):
         "lat",
         "lon"
     )
-    VALUE (
+    VALUES (
         "{element['country']}",
         "{element['name']}",
         {element['coord']['lat']},
         {element['coord']['lon']}
     );
     """
-    print(sql)
+    cursor = connect.cursor()
+    cursor.execute(sql)
+    connect.commit()
 
 
 if __name__ == '__main__':
@@ -60,8 +62,13 @@ if __name__ == '__main__':
         connection = get_connect(path)
         create_table(connection)
         data = get_data("city.list.json")
-        for element in data[:2]:
-            send_data(element)
+        for element in data[:3]:
+            try:
+                send_data(element, connection)
+            except Exception as e:
+                print("ERROR SQL")
+                print(element)
+                print(e)
         print("OK")
     else:
         print("Not arguments")
