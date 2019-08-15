b76ec24c931ff331752db0e07dc6800c4f34d3e6
Ваше Имя <hacker@google.com>
2019-08-15 20:43:18 +0300

    update

--- weather/get_country.py
+++ weather/get_country.py
@@ -11,13 +11,15 @@ def get_all_country(file_name):
             lines = html.find_all("tr")
             for line in lines:
                 rows = line.find_all("td")
-                image = rows[0].find("img").src
+                image = rows[0].find("img")
                 rus_name = rows[0].text
                 bin_code = rows[2].text
+                result.append((image.attrs["src"], rus_name, bin_code))
         return result
     else:
         raise ValueError(f"File '{file_name}' not found!")
 
 
 if __name__ == '__main__':
-    main()
+    res = get_all_country("countries.html")
+    print(res)
