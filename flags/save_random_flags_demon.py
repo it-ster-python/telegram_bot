2e142953985f7494a186c7d20f66fe8dfb3093b8
serg-yakovlev <nez91-01@radziv.local>
2019-09-07 11:44:25 +0300

    modified save_random_flags_demon

--- flags/save_random_flags_demon.py
+++ flags/save_random_flags_demon.py
@@ -1,7 +1,6 @@
 import os
 from bs4 import BeautifulSoup as Bs
 import requests
-import os
 from multiprocessing.dummy import Pool as ThPool
 from datetime import datetime
 import sys
@@ -10,24 +9,42 @@ import time
 
 
 def get_random_flags(sample_len): #makes sample of .gif file names
-    flags = []
-    result = requests.get("http://actravel.ru/country_codes.html").text
-    html = Bs(result, features="html.parser")
-    table = html.find_all("table")[0]
-    lines = table.find_all("tr")[1:]
-    results_sample = sample(lines, sample_len)     
-    for line in results_sample:
-        rows = line.find_all("td")
-        image = rows[0].find("img").attrs["src"][8:]
-        flags.append(image)
-    print(flags)
-    return flags
+    
+    def get_html():
+        try:
+            result = requests.get("http://actravel.ru/country_codes.html").text
+            html = Bs(result, features="html.parser")
+            table = html.find_all("table")[0]
+            lines = table.find_all("tr")[1:]
+            results_sample = sample(lines, sample_len)
+            flags = []
+            for line in results_sample:
+                rows = line.find_all("td")
+                image = rows[0].find("img").attrs["src"][8:]
+                flags.append(image)
+            print(flags)
+            return flags           
+        except Exception as e:
+            print("Something goes wrong, the following exception was raised:\n{0}".format(e))
+            go_further = input("Try to continue one more time? (y/n)")
+            if go_further == "n":
+                return
+            else:
+                get_html()
+
+    flags = get_html()
+    if flags == None:
+        return
+    else:
+        return flags
+
 
 path = ""
 path_split = sys.argv[0].split('\\')[:-1]
 for f in path_split:
     path = path + '\\' + f
-path = path[1:]+"\images\\"
+path = path[1:]+'\\'+'flag_images'+'\\'
+print(path)
 
 
 def save_image(img): #saves one .gif file
@@ -40,14 +57,14 @@ def save_image(img): #saves one .gif file
     with open(path+img, "wb") as f:
         f.write(img_file.content)
 
-work = True
 
 def demon():
+    #work = True    
     def worker():
         try:
-            while work:
+            while True: #while work
                 sample_len = randint(10, 20)
-                print("I'll save {0} random flag images, here are their names:".format(sample_len))
+                print("I'm going to save {0} random flag images, here are their names:".format(sample_len))
                 start = datetime.now()
                 pool = ThPool(sample_len)
                 results = pool.map(save_image, get_random_flags(sample_len))
@@ -57,25 +74,25 @@ def demon():
                 finish = datetime.now()
                 print(finish - start)
                 sleep_time = randint(5, 10)
-                print("Now I go to sleep for {0}sec., good night!\n".format(sleep_time))
+                print("Now I lay me down to sleep for {0} sec., good night!\n".format(sleep_time))
                 time.sleep(sleep_time)
-                #try:
-                 #   time.sleep(sleep_time)
-                #except KeyboardInterrupt:
-                 #   print("Something strange happend, I'll better go away. Good bye!")
-                 #   exit(1)    
-        except KeyboardInterrupt as e:
+   
+        except KeyboardInterrupt:
             try:
                 action = input("Should I have a rest? (y/n)")
                 if action == "y":
                     print("Good bye!")
-                    exit(0)
+                    return
                 else:
                     worker()
+                    #pid = os.fork()
             except:
                 worker()
-
-     worker()            
+                
+        except Exception as e:
+            print("Good bye!\nThe following exception raised in worker():\n{0}".format(e))
+            return
+    worker()
 
 
 if __name__ == '__main__':
