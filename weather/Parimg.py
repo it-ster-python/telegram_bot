04d6ca0e7445ed45f2ae6e2b84201686ba2944b3
Kiril0l <Kirill_1998_12@mail.ru>
2019-09-10 21:31:32 +0300

    New

--- weather/Parimg.py
+++ weather/Parimg.py
@@ -1,28 +1,45 @@
 import os
 from bs4 import BeautifulSoup as Bs
+import requests
+import os
+from multiprocessing.dummy import Pool as ThreadPool
+from datetime import datetime
 
+url = "http://actravel.ru/country_codes.html"
 
-def get_all_country(file_name):
-    if os.path.isfile(file_name):
-        result = []
-        with open(file_name, "r") as html_file:
-            html = Bs(html_file.read())
-            lines = html.find_all("tr")
-            for line in lines:
-                rows = line.find_all("td")
-                image = rows[0].find("img")
-                result.append((image.attrs["src"]))
-        return result
-    else:
-        raise ValueError(f"File '{file_name}' not found!")
+def get_flags():
+    flags = []
+    result = requests.get(url).text
+    html = Bs(result, features="html.parser")
+    table = html.find_all("table")[0]
+    lines = table.find_all("tr")[1:]
+    for line in lines:
+        rows = line.find_all("td")
+        image = rows[0].find("img").attrs["src"][8:]
+        flags.append(image)
+    return flags
 
-def get_flags(path):
-    pass
+path = "/home/kirill/Документы/Projects/telegram_bot/weather/flags/"
+def save_images():
+    url_templ = "http://actravel.ru/images/"
+    if not os.path.isdir(path):
+        os.mkdir(path)
+    flags = get_flags()
+    for flag in flags:
+        img_file = requests.get(url_templ + flag)
+        with open(os.path.join(path+flag), "wb") as file:
+            file.write(img_file.content)
 
-def get_sql_country(data):
-    pass
 
 
 if __name__ == '__main__':
-    res = get_all_country("countries.html")
-    print(res)
+
+    pool = ThreadPool(len(save_images()))
+    start = datetime.now()
+    results = pool.map(save_images)
+    print(len(results))
+    save_images()
+    finish = datetime.now()
+    pool.close()
+    pool.join()
+    print(finish - start)
