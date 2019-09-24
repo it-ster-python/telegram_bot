90c5b66e4aed9e9df03a09d12b3253a38fb1832e
serg-yakovlev <sergey.ya@gmail.com>
2019-09-24 21:29:50 +0300

    chat

--- network/server.py
+++ network/server.py
@@ -1,22 +1,51 @@
 import socket
+import threading as th
+
+#print(dir(socket))
 
 HOST = "127.0.0.1"
 PORT = 5000
+
+def executor(key):
+    try:
+        sock = storage[key]
+    except KeyError:
+        print(f"Key {key} not found")
+        return
+    try:
+        while True:
+            data = sock.recv(2048)
+            words = data.decode("utf-8").split()
+            words.sort()
+            if "close" in words:
+                break
+            response = "/\n".join(words)
+            for client, sock in storage.items():
+                if client != key:
+                    client_sock.send(f"{client[0]}\n".encode("utf-8"))
+                    client_sock.send(response.encode("utf-8"))
+
+            sock.send(response.encode("utf-8"))
+    except Exception as e:
+        print(e)
+    finally:
+        sock.close()
+        del storage[key]
+
+
 
 srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 srv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 srv.bind((HOST, PORT))
 srv.listen(20)
+storage = {}
 try:
-    client, address = srv.accept()
-    for index in range(1,10):
-        data = client.recv(2048)
-        data_str = data.decode("utf-8")
-        print(data)
-        if not data:
-            break
-        result = f"{data}/n{address}"
-        client.send(data)
+    flag = True
+    while flag:
+        client, addr = srv.accept()
+        storage[addr] = client
+        worker = th.thread(executor, addr)
+        worker.start()
 except Exception as e:
     print(e)
 finally:
